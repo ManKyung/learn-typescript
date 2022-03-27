d9c72b0bc4a7b62a40db46a54dfa6511be4308f5
ㅇㅇ <strikers1999@naver.com>
2022-03-27 15:16:43 +0900

    partial

---
+++ class-note/13_utility-type.ts
@@ -0,0 +1,51 @@
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  brand: string;
+  stock: number;
+}
+
+function fetchProducts(): Promise<Product[]> {
+  return new Promise((resolve) => {
+    resolve([
+      {
+        id: 1,
+        name: "상품",
+        price: 1000,
+        brand: "노브랜드",
+        stock: 20,
+      },
+    ]);
+  });
+}
+
+type ShoppingPickItem = Pick<Product, "id" | "name" | "price">;
+type ShoppingOmitItem = Omit<Product, "id" | "brand">;
+type UpdateProduct = Partial<Product>;
+
+function displayProductPickDetail(shoppingItem: ShoppingPickItem) {
+  const item: ShoppingPickItem = {
+    id: 3,
+    name: "휴지",
+    price: 1000,
+  };
+  console.log(item);
+}
+function displayProductOmitDetail(shoppingItem: ShoppingOmitItem) {
+  const item: ShoppingOmitItem = {
+    name: "휴지",
+    price: 1000,
+    stock: 30,
+  };
+  console.log(item);
+}
+
+function updateProductDetail(shoppingItem: UpdateProduct) {
+  const item: UpdateProduct = {
+    id: 4,
+    price: 1000,
+    stock: 30,
+  };
+  console.log(item);
+}
